e7d658c047385aab56c5d203668a54af2e8eb165
Leandro D. Medus <leomedus@gmail.com>
2020-06-26 12:26:32 +0200

    [mod] adding output of the script

--- 2.1_svm_mnist.py
+++ 2.1_svm_mnist.py
@@ -20,3 +20,17 @@ print("train score:", model.score(Xtrain, Ytrain), "duration:", datetime.now() -
 
 t0 = datetime.now()
 print("test score:", model.score(Xtest, Ytest), "duration:", datetime.now() - t0)
+
+
+# Outputs: 
+# model = SVC()
+# (svm) D:\Repos\courses\svm>python svm_mnist.py
+# train duration: 0:04:18.895413
+# train score: 0.9891219512195122 duration: 0:07:47.970313
+# test score: 0.985 duration: 0:00:11.319317
+
+# model = SVC(C=5., gamma=.05)
+# (svm) D:\Repos\courses\svm>python 2.1_svm_mnist.py
+# train duration: 0:14:40.831508
+# train score: 1.0 duration: 0:12:51.490385
+# test score: 0.974 duration: 0:00:18.497260

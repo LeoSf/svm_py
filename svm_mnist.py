0322107809e9fe4326254fe92221271e31f259d3
Leandro D. Medus <leomedus@gmail.com>
2020-06-26 11:28:39 +0200

    [add] basic NMIST example for classification

---
+++ svm_mnist.py
@@ -0,0 +1,21 @@
+from __future__ import print_function, division
+from builtins import range
+
+from sklearn.svm import SVC
+from util import getKaggleMNIST
+from datetime import datetime
+
+# get the data: https://www.kaggle.com/c/digit-recognizer
+Xtrain, Ytrain, Xtest, Ytest = getKaggleMNIST()
+
+model = SVC()
+
+t0 = datetime.now()
+model.fit(Xtrain, Ytrain)
+print("train duration:", datetime.now() - t0)
+
+t0 = datetime.now()
+print("train score:", model.score(Xtrain, Ytrain), "duration:", datetime.now() - t0)
+
+t0 = datetime.now()
+print("test score:", model.score(Xtest, Ytest), "duration:", datetime.now() - t0)
